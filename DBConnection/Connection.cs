868d418f7291eb57a6757af7e1e621094aeba83e
Admin <Admin@DESKTOP-U3N71MI>
2024-02-08 14:04:01 +0400

    Разработал базу данных для подключения к MySql. Леготкин

---
+++ DBConnection/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DBConnection
+{
+    public class Connection
+    {
+        public static MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection("server=LocalHost;database=pkclub;uid=root;pwd=root;");
+            connection.Open();
+
+            return connection;
+        }
+        public static MySqlDataReader Query(string Sql, MySqlConnection connection)
+        {
+            MySqlCommand command = new MySqlCommand(Sql, connection);
+            return command.ExecuteReader();
+        }
+        public static void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
